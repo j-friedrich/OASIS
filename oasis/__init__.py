6b98d99892bfab021374c7e16656426ac8f0f8a4
Johannes Friedrich <jfriedrich@flatironinstitute.org>
2023-02-15 14:27:24 -0500

    update install instructions in README

--- oasis/__init__.py
+++ oasis/__init__.py
@@ -1,3 +1,6 @@
 from .oasis_methods import oasisAR1, constrained_oasisAR1, oasisAR2, constrained_oasisAR2
 from . import functions
 from . import oasis_methods
+
+__version__ = "0.2.0"
+__author__ = 'Johannes Friedrich'
